8bd5a6329d15fd0e7809430173b98145a10cee45
myusufadiyaman <49249673+MUHAMMED-YUSUF-ADIYAMAN@users.noreply.github.com>
2021-10-26 01:18:13 +0300

    Odev02_Son

---
+++ odev02/src/index.js
@@ -0,0 +1,21 @@
+randomNumber=Math.floor(Math.random() * 3);
+        let step=2;
+       selected=(x)=>{
+            if(step==2 || step==1)
+                if(randomNumber==x)
+                {
+                    document.getElementById("img"+x).src = "https://i4.hurimg.com/i/hurriyet/75/750x0/5efd782445d2a04ed8f62cba.jpg";
+                    document.getElementById("alanId").style="display: none;";
+                    document.getElementById("kazandiId").style="display: block ;";
+                }
+                else 
+                    document.getElementById("img"+x).src = "https://imgrosetta.mynet.com.tr/file/9897198/640xauto.jpg";
+            else if(step==0)
+                {
+                    document.getElementById("alanId").style="display: none;";
+                    document.getElementById("yenildiId").style="display: block ;";
+                }
+            else if(step<0)
+                document.location.reload(true);
+            step--;
+        }
