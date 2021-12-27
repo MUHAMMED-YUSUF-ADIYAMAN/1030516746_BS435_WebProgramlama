10c4fddf192fad7653a26cd30f041cdff0d0675f
myusufadiyaman <49249673+MUHAMMED-YUSUF-ADIYAMAN@users.noreply.github.com>
2021-12-28 00:35:40 +0300

    Odev06_Son

---
+++ odev06/src/server/routes/match-api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+//const {getRandomQuizzes} = require("../db/quizzes");
+
+const router = express.Router();
+
+router.get('/matches',(req,res)=>{
+    if (!req.user) {
+        res.status(401).send();
+        return;
+    }
+
+    const payload = {
+        'name': 'Test'
+    };
+
+  //  const payload = getRandomQuizzes(3);
+    res.status(200).json(payload);
+});
+
+module.exports = router;
