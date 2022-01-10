f068a6dae8847d44025b80b9d50dd9793ab5178b
myusufadiyaman <49249673+MUHAMMED-YUSUF-ADIYAMAN@users.noreply.github.com>
2022-01-10 22:16:13 +0300

    Odev07_Son

--- odev07/src/server/server.js
+++ odev07/src/server/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const server = require('http').Server(app);
 
-const port = process.env.PORT || 300;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log('Started server on port ' + port);
